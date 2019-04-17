699f9028937ba9383a2a3d2a687634b4b60d78ba
supremeyh <supremeyh@126.com>
2019-04-17 10:05:57 +0800

    配置webpack支持less

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ npm i less-loader
 
 ### 暴露webpack配置文件
 npm run eject
+暴露出config 和 scripts 两个文件夹。此操作是不可逆，因eject功能会被删除
+
+### 配置webpack支持less
+在/config/webpack.config.js 中，依照sass，增加less变量，和 module 中的oneOf数组添加 less相应 配置
+
+
--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="App test">
+        <header className="App-header test1">
           react
         </header>
       </div>
