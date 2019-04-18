ea67209952c6c51a771157609e4f9fc4f10dbd8f
supremeyh <supremeyh@126.com>
2019-04-18 21:36:06 +0800

    时间格式化

--- src/util/util.js
+++ src/util/util.js
@@ -2,7 +2,20 @@ const Util = {
   formatDate(time) {
     if(!time) return
     let date = new Date(time)
-    return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDay() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
+    let month = date.getMonth() + 1
+    let day = date.getDay()
+    let hour = date.getHours()
+    let minute = date.getMinutes()
+    let seconds = date.getSeconds()
+    month = this.checkGreaterThanTen(month)
+    day = this.checkGreaterThanTen(day)
+    hour = this.checkGreaterThanTen(hour)
+    minute = this.checkGreaterThanTen(minute)
+    seconds = this.checkGreaterThanTen(seconds)
+    return date.getFullYear() + '-' + month + '-' + day + ' ' + hour+ ':' + minute + ':' + seconds
+  },
+  checkGreaterThanTen(num) {
+    return num = num > 10 ? num : '0' + num
   }
 }
 
