df6262f9a32ea2d1f5703c3b4602fc8da7e9bbe1
supremeyh <supremeyh@126.com>
2019-04-23 10:28:26 +0800

    modal

--- src/pages/admin/ui/button/index.js
+++ src/pages/admin/ui/button/index.js
@@ -1,14 +1,68 @@
 import React, { Component } from 'react'
-// import './index.less'
+import { Card, Button, Icon, Radio } from 'antd'
+import './index.less'
 
-class Button extends Component {
+const  ButtonGroup  = Button.Group
+const  RadioGroup  = Radio.Group
+
+
+class Buttons extends Component {
+  state ={
+    loading: true,
+    value: 2
+  }
+
+  handleLoadingClose = () => {
+    this.setState((prevState, props) => ({
+      loading: !prevState.loading
+    }))
+    // this.setState({
+    //   loading: !this.state.loading
+    // })
+  }
+
+  handleRadioChange = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
   render() {
+    const radioStyle = {
+      display: 'block',
+      height: '30px',
+      lineHeight: '30px'
+    }
+
     return (
       <div className='button'>
-        Button
+        <Card 
+          title='Title'
+          extra={<a href='#'>More</a>}
+          bordered={false}
+          >
+          <Button type='primary' icon='edit'>primary</Button>
+          <Button shape='round'>default</Button>
+          <Button shape='circle' type='dashed' icon='search'></Button>
+          <Button type='danger' disabled>danger</Button>
+          <Button icon='plus'>danger</Button>
+          <ButtonGroup>
+            <Button loading={this.state.loading} style={{marginRight: '0px'}}>loading</Button>
+            <Button type='danger' onClick={this.handleLoadingClose} >Close</Button>
+          </ButtonGroup>
+          <Icon type="scissor" />
+        </Card>
+
+        <Card title='Radio'>
+          <RadioGroup value={this.state.value} onChange={this.handleRadioChange}>
+            <Radio value={1} style={radioStyle}>A</Radio>
+            <Radio value={2} style={radioStyle}>B</Radio>
+            <Radio value={3} style={radioStyle}>C</Radio>
+          </RadioGroup>
+        </Card>
       </div>
     )
   }
 }
 
-export default Button
+export default Buttons
--- src/pages/admin/ui/modal/index.js
+++ src/pages/admin/ui/modal/index.js
@@ -1,14 +1,81 @@
 import React, { Component } from 'react'
+import { Modal, Button, Card } from 'antd'
 // import './index.less'
 
-class Modal extends Component {
+class Modals extends Component {
+  state = {
+    visible: false
+  }
+
+  showModal = (type) => {
+    this.setState({
+      [type]: true
+    })
+  }
+
+  handleOk = () => {
+    this.setState({
+      visible: false
+    })
+  }
+
+  hanleCancel = () => {
+    this.setState({
+      visible: false
+    })
+  }
+
+  showConfirm = () => {
+    Modal.confirm({
+      title: 'confirm modal',
+      content: 'some ...',
+      okType: 'dashed'
+    })
+  }
+
+  showInfo = () => {
+    Modal.info({
+      title: 'info',
+      content: 'info',
+      onOk() {
+        console.log(true)
+      }
+    })
+  }
+
+  showMethod = (type) => {
+    Modal[type]({
+      title: 'type',
+      content: 'some type',
+      onOk() {
+        console.log(type)
+      }
+    })
+  }
+ 
   render() {
     return (
       <div className='modal'>
-        Modal
+        <Card title='modal card'>
+          <Button onClick={this.showModal.bind(this, 'visible')}>show modal</Button>
+          <Button onClick={this.showConfirm}>show confirm</Button>
+          <Button onClick={this.showInfo}>show info</Button>
+          <Button onClick={() => this.showMethod('confirm')}>show method</Button>
+          <Modal 
+            title='modal'
+            visible={this.state.visible}
+            onOk={this.handleOk}
+            onCancel={this.hanleCancel}
+            okText='好了'
+            cancelText='取消'
+            style={{top: 100}}
+            >
+            <p>hhhhh</p>
+          </Modal>
+        </Card>
       </div>
     )
   }
 }
 
-export default Modal
+export default Modals
